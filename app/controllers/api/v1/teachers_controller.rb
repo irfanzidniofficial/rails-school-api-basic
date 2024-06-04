2292f94558f4d2a1fec41c0a49f000f5ed364030
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-06-04 11:30:42 +0700

    finished crud api data

--- app/controllers/api/v1/teachers_controller.rb
+++ app/controllers/api/v1/teachers_controller.rb
@@ -8,7 +8,46 @@ module Api
             end
 
             def show
+                @teacher = Teacher.find(params[:id])
+                render json: @teacher
             end
+
+            def update
+                @teacher = Teacher.find(params[:id])
+                if @teacher.update(teacher_params)
+                    render json: @teacher
+                else
+                    render json: @teacher.errors, status: unprocessable_entity
+                end
+
+            end
+
+            def destroy
+                @teacher = Teacher.find(params[:id])
+                if @teacher.present?
+                    @teacher.destroy
+                    render json: @teacher
+                else
+                    render jsoon: {error: 'Teacher not found'}
+                end
+            end
+
+            def create
+                @teacher = Teacher.new(teacher_params) 
+
+                if @teacher.save
+                    render json: @teacher
+                else
+                    render json: @teacher.errors, status: unprocessable_entity
+                end
+            end
+
+            private
+            def teacher_params
+                params.require(:teacher).permit(:name, :address)
+            end
+
+          
 
           
         end
